3951386b65553c5bdf3d8ad5e21e500aed608e84
vincent <alfa.king+git@gmail.com>
2019-02-16 16:36:33 +0800

    update roadhog mock configuration.

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -1,13 +1,7 @@
-import { delay } from 'roadhog-api-doc';
-import {user} from "./mock/user";
-
-const noProxy = process.env.NO_PROXY === 'true';
-const proxy = {
+export default {
   'POST /api/login/account': (req, res) => {
     const { password, username } = req.body;
     const isOk = password === '888888' && username === 'admin';
-    res.send({ status:  isOk ? 'ok' : 'error', type: 'account'});
+    res.send({ status: isOk ? 'ok' : 'error', type: 'account' });
   },
-  'GET /api/user': user
 };
-export default noProxy ? {} : delay(proxy, 1000);
